c9814c6467e8f3baa1da4ab1c0fc98a983745e2d
Geetika-Behl <142812594+Geetika-Behl@users.noreply.github.com>
2025-02-25 22:51:28 +0530

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import  Navbar  from "../components/Navbar/Navbar";
 import Hero from  "../components/Hero/Hero";
 import Footer from "../components/Footer/Footer";
-import FAQ from '../components/FAQ/Faq';
+import FAQ from '../components/FAQ/FAQ';
 import Pricing from '../components/Pricing/Pricing';
 
 function LandingPage() {
